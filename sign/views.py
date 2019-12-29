fae7b32b58557269a0624d91064237ad4cda4db0
大熊 <786087292@qq.com>
2019-12-29 23:56:07 +0800

    1、新增event_manage.html 文件 2、增加cookie的使用

--- sign/views.py
+++ sign/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
@@ -7,13 +7,25 @@ def index(request):
     return render(request, 'index.html')
 
 
+# 登录动作
 def login_action(request):
     if request.method == 'POST':
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
         if username == 'admin' and password == '123456':
-            return HttpResponse('login success!')
+            # return HttpResponse('login success!')
+            response = HttpResponseRedirect('/event_manage/')
+            response.set_cookie('user', username, 3600)
+            return response
+
         else:
             return render(request, 'index.html', {'error': 'username or password error'})
+
     else:
         return HttpResponse('请使用post请求!')
+
+
+# 发布会管理页
+def event_manage(request):
+    username = request.COOKIES.get('user', '')
+    return render(request, 'event_manage.html', {'user': username})
